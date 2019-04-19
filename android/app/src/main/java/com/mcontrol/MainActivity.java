51718db37f09e9d0bfb211256acaa7fe206df51a
毛贤德 <394097441@qq.com>
2019-04-19 23:00:13 +0800

    全屏，隐藏头部标题和底部键盘

--- android/app/src/main/java/com/mcontrol/MainActivity.java
+++ android/app/src/main/java/com/mcontrol/MainActivity.java
@@ -2,6 +2,14 @@ package com.mcontrol;
 
 import com.facebook.react.ReactActivity;
 
+
+//import android.app.ActionBar;
+//import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+
 public class MainActivity extends ReactActivity {
 
     /**
@@ -12,4 +20,61 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "mControl";
     }
+
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        // 设置透明状态栏
+////        if (Build.VERSION.SDK_INT >= 21) {
+////            View decorView = getWindow().getDecorView();
+////            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+////                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+////            decorView.setSystemUiVisibility(option);
+////            getWindow().setStatusBarColor(Color.TRANSPARENT);
+////        }
+//
+//        // 设置透明状态栏和透明导航栏
+//        if (Build.VERSION.SDK_INT >= 21) {
+//            View decorView = getWindow().getDecorView();
+//            int option = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+//            decorView.setSystemUiVisibility(option);
+//            getWindow().setNavigationBarColor(Color.TRANSPARENT);
+//            getWindow().setStatusBarColor(Color.TRANSPARENT);
+//        }
+//    }
+    
+    
+     
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        setStickFullScreen(getWindow().getDecorView());
+    }
+
+
+    protected void setStickFullScreen(View view) {
+        int systemUiVisibility = view.getSystemUiVisibility();
+        if (Build.VERSION.SDK_INT > 16 && Build.VERSION.SDK_INT < 19) {
+             view.setSystemUiVisibility(View.GONE);
+         } else if (Build.VERSION.SDK_INT >= 19) {
+             int flags = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                             | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                             | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                             | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                             | View.SYSTEM_UI_FLAG_FULLSCREEN
+                             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+             systemUiVisibility |= flags;
+             view.setSystemUiVisibility(systemUiVisibility);
+         }
+    }
+
+
+
 }
