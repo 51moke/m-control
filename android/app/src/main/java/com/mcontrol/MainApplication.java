e85ba41c9804b781d767b5147e33c460b8a17f9e
毛贤德 <394097441@qq.com>
2019-05-04 13:27:24 +0800

    红外线测试

--- android/app/src/main/java/com/mcontrol/MainApplication.java
+++ android/app/src/main/java/com/mcontrol/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.tcp.TcpPackage;
+import com.infrared.InfraredPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -29,7 +30,8 @@ public class MainApplication extends Application implements ReactApplication {
           new MainReactPackage(),
             new OrientationPackage(),
           new SensorManagerPackage(), // <---------- add package
-          new TcpPackage()
+          new TcpPackage(),
+              new InfraredPackage()
       );
     }
 
